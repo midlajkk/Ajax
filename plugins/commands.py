255349f4a3ffe88ebc67cd336209675b79cc33a4
midlajkk <107426425+midlajkk@users.noreply.github.com>
2022-06-13 22:08:18 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃', url=f"https://t.me/AboutAadhi")
+                InlineKeyboardButton('𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃', url=f"https://t.me/faisalkvr")
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -34,8 +34,8 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[     
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton(' HΞLᎮ ', callback_data='help'),
+            InlineKeyboardButton(' ΛBOUT ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
